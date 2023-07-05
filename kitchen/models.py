0f46526ef0c4f886adf3578b164b86a0ee9ba11f
Svitlana Savikhina <savihina111@gmail.com>
2023-07-05 11:26:18 +0300

    Create models and add AUTH_USER_MODEL to settings.py

--- kitchen/models.py
+++ kitchen/models.py
@@ -1,1 +1,38 @@
+from django.contrib.auth.models import AbstractUser
 from django.db import models
+
+
+class DishType(models.Model):
+    name = models.CharField(max_length=255, unique=True)
+
+    class Meta:
+        ordering = ["name"]
+
+    def __str__(self):
+        return self.name
+
+
+class Cook(AbstractUser):
+    years_of_experience = models.PositiveIntegerField(null=True)
+
+    class Meta:
+        verbose_name = "cook"
+        verbose_name_plural = "cooks"
+        ordering = ["username"]
+
+    def __str__(self):
+        return f"{self.username} ({self.first_name} {self.last_name})"
+
+
+class Dish(models.Model):
+    name = models.CharField(max_length=255, unique=True)
+    description = models.TextField()
+    price = models.DecimalField(max_digits=7, decimal_places=2)
+    dish_type = models.ForeignKey(DishType, on_delete=models.CASCADE)
+    cooks = models.ManyToManyField(Cook, related_name="dishes")
+
+    class Meta:
+        ordering = ["name"]
+
+    def __str__(self):
+        return self.name
