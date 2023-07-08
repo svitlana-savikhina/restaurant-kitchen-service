626e33baa0a90d09b7db82c3eca041f3c6daecff
Svitlana Savikhina <savihina111@gmail.com>
2023-07-08 11:51:51 +0300

    Create index view and inden.html for home page

---
+++ kitchen/views.py
@@ -0,0 +1,22 @@
+
+from django.shortcuts import render
+
+from kitchen.models import Cook, DishType, Dish
+
+
+def index(request):
+    num_cooks = Cook.objects.count()
+    num_dishes = Dish.objects.count()
+    num_dish_type = DishType.objects.count()
+
+    num_visits = request.session.get("num_visits", 0)
+    request.session["num_visits"] = num_visits + 1
+
+    context = {
+        "num_cooks": num_cooks,
+        "num_dishes": num_dishes,
+        "num_dish_type": num_dish_type,
+        "num_visits": num_visits + 1,
+    }
+
+    return render(request, "kitchen/index.html", context=context)
