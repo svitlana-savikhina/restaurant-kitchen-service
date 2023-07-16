200ab1dcbe6ed7ae194a483ad054f4d8e6c4001b
Svitlana Savikhina <savihina111@gmail.com>
2023-07-16 21:44:43 +0300

    Staticfiles managing

--- restaurant_kitchen_service/settings.py
+++ restaurant_kitchen_service/settings.py
@@ -46,6 +46,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     "django.middleware.security.SecurityMiddleware",
+    "whitenoise.middleware.WhiteNoiseMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
     "django.middleware.common.CommonMiddleware",
     "django.middleware.csrf.CsrfViewMiddleware",
@@ -138,7 +139,7 @@ STATIC_URL = "static/"
 
 STATICFILES_DIRS = (BASE_DIR / "static",)
 
-STATIC_ROOT = BASE_DIR / "staticfiles"
+STATIC_ROOT = "staticfiles/"
 
 
 # Default primary key field type
