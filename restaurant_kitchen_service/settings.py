e2258003a91e412a77ba1f9468878511adee7630
Svitlana Savikhina <savihina111@gmail.com>
2023-07-20 13:44:16 +0300

    Add restaurant-kitchen-service-igb6.onrender.com to ALLOWED_HOSTS

--- restaurant_kitchen_service/settings.py
+++ restaurant_kitchen_service/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = os.environ["SECRET_KEY"]
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get("DJANGO_DEBUG", "") != "False"
 
-ALLOWED_HOSTS = ["127.0.0.1"]
+ALLOWED_HOSTS = ["127.0.0.1", "restaurant-kitchen-service-igb6.onrender.com"]
 
 # Application definition
 
